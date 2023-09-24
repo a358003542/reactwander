caa4e732687b7315648d12d9496d74caf89defe9
wanze <a358003542@outlook.com>
2023-09-24 14:50:07 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return <header><h1>Header</h1></header>
+}
+function NavigationBar(){
+  return <nav>      <ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Our team</a></li>
+  <li><a href="#">Projects</a></li>
+  <li><a href="#">Contact</a></li>
+</ul></nav>
+}
+
+function SideBar(){
+  return <aside><h2>Related</h2>
+
+  <ul>
+    <li><a href="#">Oh I do like to be beside the seaside</a></li>
+    <li><a href="#">Oh I do like to be beside the sea</a></li>
+    <li><a href="#">Although in the North of England</a></li>
+    <li><a href="#">It never stops raining</a></li>
+    <li><a href="#">Oh well…</a></li>
+  </ul></aside>
+}
+
+
+function MainContent(){
+  return <main><article>
+  <h2>Article heading</h2>
+
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Donec a diam
+    lectus. Set sit amet ipsum mauris. Maecenas congue ligula as quam
+    viverra nec consectetur ant hendrerit. Donec et mollis dolor. Praesent
+    et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt
+    congue enim, ut porta lorem lacinia consectetur.
+  </p>
+
+  <h3>Subsection</h3>
+
+  <p>
+    Donec ut librero sed accu vehicula ultricies a non tortor. Lorem ipsum
+    dolor sit amet, consectetur adipisicing elit. Aenean ut gravida lorem.
+    Ut turpis felis, pulvinar a semper sed, adipiscing id dolor.
+  </p>
+
+  <p>
+    Pelientesque auctor nisi id magna consequat sagittis. Curabitur
+    dapibus, enim sit amet elit pharetra tincidunt feugiat nist imperdiet.
+    Ut convallis libero in urna ultrices accumsan. Donec sed odio eros.
+  </p>
+
+  <h3>Another subsection</h3>
+
+  <p>
+    Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum
+    soclis natoque penatibus et manis dis parturient montes, nascetur
+    ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem
+    facilisis semper ac in est.
+  </p>
+
+  <p>
+    Vivamus fermentum semper porta. Nunc diam velit, adipscing ut
+    tristique vitae sagittis vel odio. Maecenas convallis ullamcorper
+    ultricied. Curabitur ornare, ligula semper consectetur sagittis, nisi
+    diam iaculis velit, is fringille sem nunc vet mi.
+  </p>
+</article></main>
+}
+function Footer(){
+  return <footer><p>©Copyright 2050 by nobody. All rights reversed.</p></footer>
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return [
+    <Header />,
+    <NavigationBar />,
+    <SideBar />,
+    <MainContent />,
+    <Footer />,
+  ];
 }
 
 export default App;
